ddc064b95d2effe4aaa7373158013f9215a52488
Karishma <karishma.7022@gmail.com>
2026-05-04 16:01:59 +0530

    feat: auto-focus search input with useRef

--- vite-project/src/components/SearchBar.jsx
+++ vite-project/src/components/SearchBar.jsx
@@ -1,10 +1,21 @@
-import {useState} from "react";
+import {useState,useEffect,useRef} from "react";
+
+/*In SearchBar.jsx, use useRef + useEffect together to auto-focus the input when the page loads.
+Think about:
+Where does useRef get created?
+Where does the ref get attached?
+When should .focus() be called — and which hook handles "when component first loads"?*/
 
 function SearchBar({onSearch}){
     const [inputText,setinputText] = useState("");
+    const inputRef = useRef(null);
+
+    useEffect(()=>{
+        inputRef.current.focus();
+    },[]);
     return(
         <>
-            <input value={inputText} type="text" placeholder="type your city..." onChange={e=>{setinputText(e.target.value)}}></input>
+            <input value={inputText} ref={inputRef} type="text" placeholder="type your city..." onChange={e=>{setinputText(e.target.value)}}></input>
             <button onClick={()=>{onSearch(inputText)}}> Search </button>
         </>
     )
